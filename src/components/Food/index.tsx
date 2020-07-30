3b1192ba198f96555a1d9feea6c18f960796c0d0
willmorim <pdbawilliam@gmail.com>
2020-07-29 21:10:20 -0300

    desafio GoRestaurant web

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
+import api from '../../services/api';
 
 interface IFoodPlate {
   id: number;
@@ -27,11 +28,15 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    })
+    setIsAvailable(!isAvailable)
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
